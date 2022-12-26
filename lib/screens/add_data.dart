93bcbe504eade26b7466f1255d7dececb032e0f6
Setiawanak <adekukuh3012@gmail.com>
2022-12-27 00:51:05 +0700

    update

---
+++ lib/screens/add_data.dart
@@ -0,0 +1,80 @@
+import 'dart:convert';
+
+import 'package:data_nasabah/screens/homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class AddData extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _noRekController = TextEditingController();
+  TextEditingController _noKTPController = TextEditingController();
+
+  Future saveData() async {
+    final response =
+        await http.post(Uri.parse("http://192.168.1.2:8000/api/data"), body: {
+      "name": _nameController.text,
+      'noRek': _noRekController.toString(),
+      'noKTP': _noKTPController.toString(),
+    });
+    print(response.body);
+    return json.decode(response.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Add Data Baru"),
+        ),
+        body: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Masukan data nama !";
+                      }
+                      return null;
+                    },
+                    controller: _nameController,
+                    decoration: InputDecoration(labelText: "Name")),
+                TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Masukan data rekening !";
+                      }
+                      return null;
+                    },
+                    controller: _noRekController,
+                    decoration: InputDecoration(labelText: "No Rekening")),
+                TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Masukan data KTP !";
+                      }
+                      return null;
+                    },
+                    controller: _noKTPController,
+                    decoration: InputDecoration(labelText: "No KTP")),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        saveData().then((value) {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: ((context) => HomePage())));
+                        });
+                      }
+                      ;
+                    },
+                    child: Text("Save"))
+              ],
+            )));
+  }
+}
---
+++ lib/screens/homepage.dart
@@ -0,0 +1,102 @@
+import 'dart:convert';
+import 'dart:ffi';
+
+import 'package:data_nasabah/screens/add_data.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  final String url = 'http://192.168.1.2:8000/api/data';
+
+  Future getData() async {
+    var response = await http.get(Uri.parse(url));
+    print(json.decode(response.body));
+    return json.decode(response.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    getData();
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => AddData()));
+          },
+          child: Icon(Icons.add),
+        ),
+        appBar: AppBar(
+          title: Text('Data Nasabah'),
+        ),
+        body: FutureBuilder(
+            future: getData(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                    itemCount: snapshot.data['data'].length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        height: 75,
+                        child: Card(
+                          elevation: 3,
+                          child: Row(
+                            children: [
+                              GestureDetector(
+                                /* onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => DataDetail(
+                                                Data: snapshot.data['data']
+                                                    [index],
+                                              )));
+                                },*/
+                                child: Container(
+                                    padding: EdgeInsets.all(10),
+                                    width: 90,
+                                    child: Text(
+                                        snapshot.data['data'][index]['name'])),
+                              ),
+                              //Text(snapshot.data['data'][index]['noRek']),
+                              Expanded(
+                                child: Container(
+                                  padding: EdgeInsets.all(10),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Icon(Icons.edit),
+                                      Icon(Icons.delete),
+                                      Text(
+                                        snapshot.data['data'][index]['noRek']
+                                            .toString(),
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      Text(
+                                        snapshot.data['data'][index]['noKtp']
+                                            .toString(),
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    });
+              } else {
+                return Text('Data Error');
+              }
+            }));
+  }
+}
